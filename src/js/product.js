8e2039b3ff9fd7e63e5880c861dc9bce0ea744e0
Talionidas <114085237+Talionidas@users.noreply.github.com>
2026-09-16 20:27:12 +0200

    Revert "Week 2 + individual"

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,19 @@
-import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
-const productID = getParam("product");
+const dataSource = new ProductData('tents');
 
-const product = new ProductDetails(productID, dataSource);
-product.init();
+function addProductToCart(product) {
+  setLocalStorage('so-cart', product);
+}
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
