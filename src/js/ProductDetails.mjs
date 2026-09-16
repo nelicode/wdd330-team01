d4904a122c749f3a76fd68cd2a40bec8e4e61b24
Thierry Stahl <talionidas@hotmail.com>
2026-09-16 11:50:06 +0200

    Discount

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -37,9 +37,27 @@ function productDetailsTemplate(product) {
   productImage.src = product.Image;
   productImage.alt = product.NameWithoutBrand;
 
-  document.getElementById("productPrice").textContent = product.FinalPrice;
+  document.getElementById("productPrice").textContent = `$${product.FinalPrice.toFixed(2)}`;
   document.getElementById("productColor").textContent = product.Colors[0].ColorName;
   document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
+
+  const retailPrice = document.getElementById("retailPrice");
+  const discount = document.getElementById("discount");
+
+  if (product.FinalPrice < product.SuggestedRetailPrice) {
+    const discountPercent = Math.round(
+      ((product.SuggestedRetailPrice - product.FinalPrice) /
+        product.SuggestedRetailPrice) *
+        100
+    );
+
+    retailPrice.textContent = `Retail Price: $${product.SuggestedRetailPrice.toFixed(2)}`;
+    discount.textContent = `${discountPercent}% OFF`;
+  } 
+  else {
+    retailPrice.textContent = "";
+    discount.textContent = "";
+  }
 
   document.getElementById("addToCart").dataset.id = product.Id;
 }
