09fb3f9604593474108f92c0c7659dd5a45efb78
Thierry Stahl <talionidas@hotmail.com>
2026-09-18 21:07:55 +0200

    expand inventory

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,22 +1,26 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `/json/${category}.json`;
+  constructor() {
   }
-
-  async getData() {
-    const response = await fetch(this.path);
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } 
-    else {
-      throw new Error("Bad Response");
-    }
+  async getData(category) {
+    const response = await fetch(`${baseURL}products/search/${category}`);
+    const data = await convertToJson(response);
+    
+    return data.Result;
   }
-
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(`${baseURL}product/${id}`);
+    const data = await convertToJson(response);
+    console.log(data.Result);
+    return data.Result;
   }
 }
--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -30,11 +30,12 @@ export default class ProductDetails {
 }
 
 function productDetailsTemplate(product) {
-  document.querySelector("h2").textContent = product.Brand.Name;
-  document.querySelector("h3").textContent = product.NameWithoutBrand;
+  document.querySelector("h2").textContent = product.Category.charAt(0).toUpperCase() + product.Category.slice(1);
+  document.querySelector("#p-brand").textContent = product.Brand.Name;
+  document.querySelector("#p-name").textContent = product.NameWithoutBrand;
 
-  const productImage = document.getElementById("productImage");
-  productImage.src = product.Image;
+  const productImage = document.querySelector("#p-image");
+  productImage.src = product.Images.PrimaryExtraLarge;
   productImage.alt = product.NameWithoutBrand;
 
   document.getElementById("productPrice").textContent = `$${product.FinalPrice.toFixed(2)}`;
