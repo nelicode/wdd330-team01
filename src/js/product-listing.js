611cd61445c0d584379caa956d6b6ae8e93ac1c6
nelicode <nelida11colque22@gmail.com>
2026-09-25 15:29:21 -0500

    Fix duplicate import in product-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,5 +1,5 @@
 import ExternalServices from "./ExternalServices.mjs";
-import ExternalServices from "./ExternalServices.mjs";
+import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
